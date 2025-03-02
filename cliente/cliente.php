08d7d1c889002b1897763150e7dd3025fdfc3ecf
Manuu <125753742+Manusm2712@users.noreply.github.com>
2025-03-01 20:11:20 -0500

    cliente

--- cliente/cliente.php
+++ cliente/cliente.php
@@ -3,7 +3,7 @@ include "../includes/header.php";
 ?>
 
 <!-- TÍTULO. Cambiarlo, pero dejar especificada la analogía -->
-<h1 class="mt-3">Entidad análoga a CLIENTE (NOMBRE)</h1>
+<h1 class="mt-3">Entidad análoga a TALLER (CLIENTE)</h1>
 
 <!-- FORMULARIO. Cambiar los campos de acuerdo a su trabajo -->
 <div class="formulario p-4 m-3 border rounded-3">
@@ -11,18 +11,13 @@ include "../includes/header.php";
     <form action="cliente_insert.php" method="post" class="form-group">
 
         <div class="mb-3">
-            <label for="cedula" class="form-label">Cédula</label>
-            <input type="number" class="form-control" id="cedula" name="cedula" required>
+            <label for="id" class="form-label">ID</label>
+            <input type="number" class="form-control" id="id" name="id" required>
         </div>
 
         <div class="mb-3">
             <label for="nombre" class="form-label">Nombre</label>
             <input type="text" class="form-control" id="nombre" name="nombre" required>
-        </div>
-
-        <div class="mb-3">
-            <label for="celular" class="form-label">Celular</label>
-            <input type="number" class="form-control" id="celular" name="celular" required>
         </div>
 
         <button type="submit" class="btn btn-primary">Agregar</button>
@@ -47,10 +42,8 @@ if($resultadoCliente and $resultadoCliente->num_rows > 0):
         <!-- Títulos de la tabla, cambiarlos -->
         <thead class="table-dark">
             <tr>
-                <th scope="col" class="text-center">Cédula</th>
+                <th scope="col" class="text-center">ID</th>
                 <th scope="col" class="text-center">Nombre</th>
-                <th scope="col" class="text-center">Celular</th>
-                <th scope="col" class="text-center">Acciones</th>
             </tr>
         </thead>
 
@@ -64,18 +57,8 @@ if($resultadoCliente and $resultadoCliente->num_rows > 0):
             <!-- Fila que se generará -->
             <tr>
                 <!-- Cada una de las columnas, con su valor correspondiente -->
-                <td class="text-center"><?= $fila["cedula"]; ?></td>
+                <td class="text-center"><?= $fila["id"]; ?></td>
                 <td class="text-center"><?= $fila["nombre"]; ?></td>
-                <td class="text-center"><?= $fila["celular"]; ?></td>
-                
-                <!-- Botón de eliminar. Debe de incluir la CP de la entidad para identificarla -->
-                <td class="text-center">
-                    <form action="cliente_delete.php" method="post">
-                        <input hidden type="text" name="cedulaEliminar" value="<?= $fila["cedula"]; ?>">
-                        <button type="submit" class="btn btn-danger">Eliminar</button>
-                    </form>
-                </td>
-
             </tr>
 
             <?php
